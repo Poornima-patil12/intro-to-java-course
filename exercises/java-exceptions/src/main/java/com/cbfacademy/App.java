5e20efe002a900b4dfbda38e3dbd64c7748cdf05
Poornima <Poornimapatil124@gmail.com>
2024-08-15 14:52:09 +0100

    homework

--- exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/App.java
@@ -1,5 +1,7 @@
 package com.cbfacademy;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<String> fileNames = Arrays.asList("App.java", "App.txt", null, "App.md");
+        FileExtension fileExtension = new FileExtension();
+        Map<String, Integer> resultMap = fileExtension.map(fileNames);
+
+        // Print the result map
+        for (Map.Entry<String, Integer> entry : resultMap.entrySet()) {
+            System.out.println("File: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
     }
 }
