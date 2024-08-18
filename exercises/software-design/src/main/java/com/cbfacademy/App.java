b6dc4877fb1a3947c855345078b65c53c36911e9
Poornima <Poornimapatil124@gmail.com>
2024-08-18 21:47:20 +0100

    homework

--- exercises/software-design/src/main/java/com/cbfacademy/App.java
+++ exercises/software-design/src/main/java/com/cbfacademy/App.java
@@ -2,19 +2,33 @@ package com.cbfacademy;
 
 public class App {
     public static void main(String[] args) {
-        Game game = GameFactory.create();
+        // Game game = GameFactory.create();
+        SimpleContainer container = initialiseContainer();
+        Game game = container.get(Game.class);
+
         String winner = game.play();
-        Player player1 = PlayerFactory.create();
-        Player player2 = PlayerFactory.create();
-        Game dice = new DiceGame(player1, player2);
-        // winner=player1.();
-        dice.play();
+        // Player player1;
+        // Player player2;
+        // Game dice = new DiceGame(player1, player2);
+
+        // dice.play();
 
         if (winner == null) {
             System.out.println("It's a draw!");
         } else {
             System.out.println(winner + " wins!");
         }
+
+    }
+
+    private static SimpleContainer initialiseContainer() {
+        SimpleContainer container = new SimpleContainer();
+
+        // Register mappings for any required interfaces with their concrete
+        // implementations
+        container.register(Game.class, DiceGame.class);
+        container.register(Player.class, DicePlayer.class);
+        return container;
     }
 
 }
