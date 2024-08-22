c180c06f9683554a4d8f6f56cfb6c2f6e67ede1e
Poornima <Poornimapatil124@gmail.com>
2024-08-22 17:49:37 +0100

    Task

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -2,10 +2,14 @@ package com.cbfacademy;
 
 public class FizzBuzz {
 
-    public static String get(int n) {
+    public static String get(Integer n) {
         // throw new RuntimeException("Not implemented");
+        if (n % 3 == 0 && n % 5 == 0)
+            return "FizzBuzz";
+        if (n % 5 == 0)
+            return "Buzz";
         if (n % 3 == 0)
             return "Fizz";
-        return "1";
+        return n.toString();
     }
 }
--- exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTest.java
@@ -1,35 +1,42 @@
 package com.cbfacademy;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.CoreMatchers.is;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class FizzBuzzTest {
-    @Test
-    @DisplayName("returns 1 when input is 1")
 
-    public void printsFizz1() {
-        final String actual = FizzBuzz.get(1);
-        assertThat(actual, is("1"));
+    static Stream<Arguments> InputAndOutput() {
+        return Stream.of(arguments(1, "1"),
+                arguments(2, "2"),
+                arguments(3, "Fizz"),
+                arguments(4, "4"),
+                arguments(5, "Buzz"),
+                arguments(6, "Fizz"),
+                arguments(7, "7"),
+                arguments(8, "8"),
+                arguments(9, "Fizz"),
+                arguments(10, "Buzz"),
+                arguments(11, "11"),
+                arguments(12, "Fizz"),
+                arguments(13, "13"),
+                arguments(14, "14"),
+                arguments(15, "FizzBuzz"));
     }
 
-    @Test
-    @DisplayName("returns Fizz when input is 3")
+    @ParameterizedTest
+    @MethodSource("InputAndOutput")
+    @DisplayName("return EXPECTED when input is BETWEEN 1 to 15")
 
-    public void printsFizz3() {
-        final String actual = FizzBuzz.get(3);
-        assertThat(actual, is("Fizz"));
-
+    public void printsFizzBuzz(Integer n, String expected) {
+        final String actual = FizzBuzz.get(n);
+        assertThat(actual, is(expected));
     }
 
-    @Test
-    @DisplayName("returns Fizz when input is 6")
-
-    public void printsFizz6() {
-        final String actual = FizzBuzz.get(6);
-        assertThat(actual, is("Fizz"));
-
-    }
 }
